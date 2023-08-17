15bfb60b4b08e4f781d59bc165338d72d665aebb
Hyun Kim <115480269+cookiehyee@users.noreply.github.com>
2023-08-17 14:37:44 -0400

    Add files via upload

---
+++ src/wordfisher/WordFisher.java
@@ -0,0 +1,151 @@
+package lab13;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class WordFisher {
+
+	// Please note these variables. they are the state of the object.
+	public HashMap<String, Integer> vocabulary;
+	public List<String> stopwords; // User ArrayList for initialization
+	private String inputTextFile;
+	private String stopwordsFile;
+
+	WordFisher(String inputTextFile, String stopwordsFile) throws IOException {
+		this.inputTextFile = inputTextFile;
+		this.stopwordsFile = stopwordsFile;
+
+		buildVocabulary();
+		getStopwords();
+	}
+
+	public void buildVocabulary() throws IOException {
+		vocabulary = new HashMap<String, Integer>();
+
+		String reader = new String(Files.readAllBytes(Paths.get(inputTextFile)));
+		String[] allWords = reader.split("\\s+");
+		
+
+		for(int i = 0; i < allWords.length; i++) {
+			allWords[i] = allWords[i].replaceAll("[^a-zA-Z0-9 ]", "");
+			allWords[i] = allWords[i].toLowerCase();
+		}
+	
+		
+		for(int i = 0; i < allWords.length; i++) {
+			if (vocabulary.containsKey(allWords[i])) {
+				vocabulary.replace(allWords[i], vocabulary.get(allWords[i]) + 1);
+			}
+			else {
+				vocabulary.put(allWords[i], 1);
+			}
+		}
+	
+
+	}
+
+	public void getStopwords() {
+		stopwords = new ArrayList<String>();
+		String word;
+
+		try {
+			BufferedReader input = new BufferedReader(new FileReader(stopwordsFile));
+			while ((word = input.readLine()) != null) {
+				stopwords.add(word);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public int getWordCount() {
+		
+		int wordCount = 0;
+		
+		for(String key : vocabulary.keySet()) {	
+			wordCount += vocabulary.get(key);
+		}
+		
+		return wordCount - vocabulary.get("");
+	}
+
+	public int getNumUniqueWords() {
+		return vocabulary.size();
+	}
+
+	public int getFrequency(String word) {
+		if(vocabulary.containsKey(word)){
+			return vocabulary.get(word);
+		}
+		return -1;
+	}
+
+	public void pruneVocabulary() {
+		for(int i = 0; i < stopwords.size(); i++) {
+			vocabulary.remove(stopwords.get(i));
+		}
+	}
+	
+	public class WordNode {
+		String word;
+		int frequency;
+		
+		WordNode(String word, int frequency) {
+			this.word = word;
+			this.frequency = frequency;
+		}
+
+	}
+	
+	public class WordNodeComparator implements Comparator<WordNode> {
+		@Override
+		public int compare(WordNode o1, WordNode o2) {
+			return o2.frequency - o1.frequency;
+		}
+	}
+
+	public ArrayList<String> getTopWords(int n) {
+		ArrayList<String> topWords = new ArrayList<String>();
+		PriorityQueue<WordNode> wordNodes = new PriorityQueue<WordNode>(new WordNodeComparator());
+		
+		for(String word : vocabulary.keySet()) {
+			if(!word.equals("")) {
+				wordNodes.add(new WordNode(word, vocabulary.get(word)));
+			}
+		}
+		
+		for(int i = 0; i < n; i++) {
+			topWords.add(wordNodes.poll().word);
+		}
+		
+		return topWords;
+	}
+
+	public ArrayList<String> commonPopularWords(int n, WordFisher other) {
+		ArrayList<String> commonPopWords = new ArrayList<String>();
+		ArrayList<String> thisWordFisher = new ArrayList<String>();
+		ArrayList<String> otherWordFisher = new ArrayList<String>();
+		
+		thisWordFisher = this.getTopWords(n);
+		otherWordFisher = other.getTopWords(n);
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				if(thisWordFisher.get(i).equals(otherWordFisher.get(j))) {
+					commonPopWords.add(thisWordFisher.get(i));
+				}
+			}
+		}
+		
+		return commonPopWords;
+	}
+
+}
---
+++ src/wordfisher/WordFisherTester.java
@@ -0,0 +1,44 @@
+package lab13;
+
+import java.io.IOException;
+
+public class WordFisherTester {
+	
+	public static void main(String[] args) {
+		
+		try {
+			
+			WordFisher alice = new WordFisher("texts/carroll-alice.txt", "stopwords.txt");
+			WordFisher moby = new WordFisher("texts/moby-dick.txt", "stopwords.txt");
+			
+			
+			System.out.println("Number of words in Alice: " + alice.getWordCount());
+			System.out.println("Number of words in Moby: " + moby.getWordCount());
+			
+			System.out.println("\nNumber of unique words in Alice: " + alice.getNumUniqueWords());
+			System.out.println("Number of unique words in Moby: " + moby.getNumUniqueWords());
+			
+			System.out.println("\nFrequency of the word handkerchief in Alice: " + alice.getFrequency("handkerchief"));
+			System.out.println("Frenquency of the word handkerchief in Moby: " + moby.getFrequency("handkerchief"));
+			
+			alice.pruneVocabulary();
+			System.out.println("\nStop words removed from Alice.");
+			System.out.println("Number of words in Alice after pruning: " + alice.getWordCount());
+			moby.pruneVocabulary();
+			System.out.println("Stop words removed from Moby.");
+			System.out.println("Number of words in Moby after pruning: " + moby.getWordCount());
+			
+			
+			System.out.println("\nTop 10 words of pruned Alice: " + alice.getTopWords(10));
+			System.out.println("Top 10 words of pruned Moby: " + moby.getTopWords(10));
+			
+			System.out.println("\nCommon popular words of Alice and Moby: " + alice.commonPopularWords(20, moby));
+			
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+}
